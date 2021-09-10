17bde39274607e1a54f894df41d691f71ae1af37
md82 <md82@tistory.com>
2021-09-10 22:28:36 +0900

    init guestBook Project

--- src/main/java/org/zerock/ex3/GuestBookApplication.java
+++ src/main/java/org/zerock/ex3/GuestBookApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Ex3Application {
+public class GuestBookApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Ex3Application.class, args);
+        SpringApplication.run(GuestBookApplication.class, args);
     }
 
 }
--- src/main/java/org/zerock/ex3/ServletInitializer.java
+++ src/main/java/org/zerock/ex3/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Ex3Application.class);
+        return application.sources(GuestBookApplication.class);
     }
 
 }
